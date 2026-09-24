c5e2da7620bec151155227ae137dc3e373f8a3e7
亘 塚本 <wataru.09232007@outlook.jp>
2026-09-24 14:29:01 +0900

    3コマ目

---
+++ jankenkazuate/janken.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include "janken.h"
+using namespace std;
+
+void InitRand()
+{
+	srand((unsigned int)time(NULL));
+}
+
+int player()
+{
+	int te;
+	cout << "手を入力してください: (0)グー (1)チョキ (2)パー" << endl;
+	cin >> te;
+	te == 0 ? cout << "あなたの手はグーです" << endl : te == 1 ? cout << "あなたの手はチョキです" << endl : cout << "あなたの手はパーです" << endl;
+	return te;
+}
+
+int janken()
+{
+	int npc;
+	npc = rand() % 3;
+	npc == 0 ? cout << "グー" << endl : npc == 1 ? cout << "チョキ" << endl : cout << "パー" << endl;
+	return npc;
+}
+
+int judge(int player, int npc)
+{
+	if (player == npc)
+	{
+		cout << "あいこです" << endl;
+		return 0;
+	}
+	else if ((player == 0 && npc == 1) || (player == 1 && npc == 2) || (player == 2 && npc == 0))
+	{
+		cout << "あなたの勝ちです" << endl;
+		return 1;
+	}
+	else
+	{
+		cout << "あなたの負けです" << endl;
+		return -1;
+	}
+}
--- jankenkazuate/jankenkazuate.cpp
+++ jankenkazuate/jankenkazuate.cpp
@@ -1,10 +1,25 @@
 ﻿#include <iostream>
+#include "janken.h"
 using namespace std;
+
+
+void start()
+{
+	int n;
+	cout << "数字を入力してください: ";
+	cout << "(1)じゃんけんゲーム" << "(2)数当てゲーム" << "(3)終了" << endl;
+	cin >> n;
+	n == 1 ? cout << "じゃんけんゲームを開始します" << endl : 
+	n == 2 ? cout << "数当てゲームを開始します" << endl :
+	n == 3 ? cout << "終了します" << endl :
+			 cout << "無効な入力です" << endl;
+	
+}
 
 int main()
 {
-   cout << "Hello Worlttttt";
-	cout << "Hello World!\n";
-
+	start();
+	InitRand();
+	janken();
 }
 
